a6915e55613e301523877342e204603c14ee6368
t-ilieva <ilieva0249@gmail.com>
2023-02-06 12:16:56 +0200

    commit

--- course/HodViews.py
+++ course/HodViews.py
@@ -11,10 +11,10 @@ from course.models import User, Teacher, Course, Category, Student
 def admin_home(request):
     return render(request, "hod_templates/home_content.html")
 
-def add_teachers(request):
-    return render(request, "hod_templates/add_teachers_template.html")
+def add_teacher(request):
+    return render(request, "hod_templates/add_teacher_template.html")
 
-def add_teachers_save(request):
+def add_teacher_save(request):
     if request.method != "POST":
         return HttpResponse("Method Not Allowed")
     else:
@@ -29,7 +29,60 @@ def add_teachers_save(request):
             user.teacher.address=address
             user.save()
             messages.success(request,"Successfully Added Teacher")
-            return HttpResponseRedirect("/add_teachers")
+            return HttpResponseRedirect("/add_teacher")
         except:
             messages.error(request,"Failed to Add Teacher")
-            return HttpResponseRedirect("/add_teachers")
+            return HttpResponseRedirect("/add_teacher")
+        
+def manage_teachers(request):
+    teachers=Teacher.objects.all()
+    return render(request,"hod_templates/manage_teachers_template.html",{"teachers":teachers})
+
+def edit_teacher(request, teacher_id):
+    teacher=Teacher.objects.get(user=teacher_id)
+    return render(request,"hod_templates/edit_teacher_template.html",{"teacher":teacher ,"id":teacher_id})
+
+def edit_teacher_save(request):
+    if request.method != "POST":
+        return HttpResponse("<h2>Method Not Allowed</h2>")
+    else:
+        teacher_id=request.POST.get("teacher_id")
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        email=request.POST.get("email")
+        username=request.POST.get("username")
+        address=request.POST.get("address")
+
+        try:
+            user=User.objects.get(id=teacher_id)
+            user.first_name=first_name
+            user.last_name=last_name
+            user.email=email
+            user.username=username
+            user.save()
+
+            teacher_model=Teacher.objects.get(user=teacher_id)
+            teacher_model.address=address
+            teacher_model.save()
+            messages.success(request,"Successfully Edited Teacher")
+            return HttpResponseRedirect(reverse("edit_teacher", kwargs={"teacher_id":teacher_id}))
+        except:
+            messages.error(request,"Failed to Edit Teacher")
+            return HttpResponseRedirect(reverse("edit_teacher", kwargs={"teacher_id":teacher_id}))
+        
+def add_category(request):
+    return render(request,"hod_templates/add_category_template.html")
+
+def add_category_save(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        category=request.POST.get("category")
+        try:
+            category_model=Category(category_name=category)
+            category_model.save()
+            messages.success(request,"Successfully Added Category")
+            return HttpResponseRedirect("add_category")
+        except:
+            messages.error(request,"Failed To Add Category")
+            return HttpResponseRedirect("add_category")
--- web_project/urls.py
+++ web_project/urls.py
@@ -28,6 +28,28 @@ urlpatterns = [
     path('get_user_details', views.GetUserDetails),
     path('logout_user', views.logout_user),
     path('admin_home', HodViews.admin_home),
-    path('add_teachers', HodViews.add_teachers),
-    path('add_teachers_save', HodViews.add_teachers_save),
+
+    path('add_teacher', HodViews.add_teacher),
+    path('add_teacher_save', HodViews.add_teacher_save),
+    path('manage_teachers', HodViews.manage_teachers, name="manage_teachers"),
+    path('edit_teacher/<str:teacher_id>', HodViews.edit_teacher, name="edit_teacher"),
+    path('edit_teacher_save', HodViews.edit_teacher_save, name="edit_teacher_save"),
+
+    path('add_category', HodViews.add_category),
+    path('add_category_save', HodViews.add_category_save),
+    path('manage_categories', HodViews.manage_categories, name="manage_categories"),
+    path('edit_category/<str:category_id>', HodViews.edit_category, name="edit_category"),
+    path('edit_category_save', HodViews.edit_category_save, name="edit_category_save"),
+
+    path('add_course', HodViews.add_course,name="add_course"),
+    path('add_course_save', HodViews.add_course_save,name="add_course_save"),
+    path('manage_courses', HodViews.manage_courses, name="manage_courses"),
+    path('edit_course/<str:course_id>', HodViews.edit_course,name="edit_course"),
+    path('edit_course_save', HodViews.edit_course_save,name="edit_course_save"),
+
+    path('add_student', HodViews.add_student,name="add_student"),
+    path('add_student_save', HodViews.add_student_save, name="add_student_save"),
+    path('manage_students', HodViews.manage_students, name="manage_students"),
+    path('edit_student/<str:student_id>', HodViews.edit_student, name="edit_student"),
+    path('edit_student_save', HodViews.edit_student_save, name="edit_student_save"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
